6a11ba5b936689257005942c91d752429bdce46e
d116626 <d116626@gmail.com>
2025-08-19 11:32:49 -0300

    feat: remove some fields from create_dump_db_schedules

--- iplanrio/pipelines_utils/prefect.py
+++ iplanrio/pipelines_utils/prefect.py
@@ -311,14 +311,6 @@ def create_dump_db_schedules(
     base_anchor_date_str: str,
     runs_interval_minutes: int,
     timezone: str,
-    db_type: str,
-    db_database: str,
-    db_host: str,
-    db_port: int,
-    dataset_id: str,
-    infisical_secret_path: str,
-    default_biglake_table: bool = True,
-    default_batch_size: int = 50000,
 ):
     """
     Generates a full Prefect deployment YAML for database dump tasks.
@@ -331,19 +323,6 @@ def create_dump_db_schedules(
         base_anchor_date_str (str): The anchor date for the first schedule.
         runs_interval_minutes (int): The number of minutes to wait between starting each schedule.
         timezone (str): The IANA timezone for all schedules.
-        db_type (str): The database type (e.g., 'oracle').
-        db_database (str): The name of the database.
-        db_host (str): The database host.
-        db_port (int): The database port.
-        dataset_id (str): The default dataset ID for the dumps.
-        infisical_secret_path (str): The path to secrets in Infisical.
-        work_pool_name (str): The name of the work pool.
-        work_queue_name (str): The name of the work queue.
-        job_image (str): The Docker image for the job.
-        job_command (str): The command to execute the flow run.
-        default_biglake_table (bool): The default value for 'biglake_table'.
-        default_batch_size (int): The default value for 'batch_size'.
-
     Returns:
         dict: A dictionary representing the complete Prefect deployment YAML.
     """
@@ -355,16 +334,7 @@ def create_dump_db_schedules(
         anchor_date = base_anchor_date + timedelta(minutes=runs_interval_minutes * i)
 
         # Start with a base set of parameters for the flow run
-        flow_run_parameters = {
-            "db_type": db_type,
-            "db_database": db_database,
-            "db_host": db_host,
-            "db_port": str(db_port),
-            "dataset_id": table_params.get("dataset_id", dataset_id),
-            "infisical_secret_path": infisical_secret_path,
-            "biglake_table": default_biglake_table,
-            "batch_size": default_batch_size,
-        }
+        flow_run_parameters = {}
 
         # Merge the specific parameters for this table
         # This includes table_id, execute_query, dump_mode, etc.
